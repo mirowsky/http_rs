4cf844b1cbb87b1fc1a31a075c594d4681f91ad0
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-07-03 22:03:05 -0300

    feat(body_parser): alternate implementation of body_parser without using regex crate

--- src/http_parser/body_parser.rs
+++ src/http_parser/body_parser.rs
@@ -8,6 +8,37 @@ lazy_static! {
     static ref BODY_REGEX: Regex = Regex::new(r"(?im)^\s*\{{1}[\s\S]*\}{1}$").unwrap();
 }
 
+pub fn body_parser_alt(raw_response: &str) -> Result<HashMap<String, String>> {
+    if let Some(body) = raw_response.split("\r\n\r\n").nth(1) {
+        let body = body.trim();
+        let body = body.trim_end_matches('}');
+        let body = body.trim_start_matches('{');
+
+        let body = body
+            .split(',')
+            .map(|x| {
+                let mut x = x.splitn(2, ':');
+                let key = x
+                    .next()
+                    .unwrap()
+                    .trim()
+                    .trim_start_matches('\"')
+                    .trim_end_matches('\"');
+                let value = x
+                    .next()
+                    .unwrap()
+                    .trim()
+                    .trim_start_matches('\"')
+                    .trim_end_matches('\"');
+                (key.to_string(), value.to_string())
+            })
+            .collect::<HashMap<_, _>>();
+
+        Ok(body)
+    } else {
+        Err(color_eyre::eyre::eyre!("Could not parse body"))
+    }
+}
 pub fn body_parser(raw_response: &str) -> Result<HashMap<String, String>> {
     let body = BODY_REGEX
         .find(raw_response)
@@ -42,6 +73,17 @@ fn remove_non_alphanumeric(input: &str) -> String {
 mod tests {
     use super::*;
 
+    const RAW_RESPONSE_MOCK: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 12\r\n\r\n{\"hello\":\"world\",\"foo\":\"bar\",\"baz\":\"qux\",\"quux\":\"corge\"}";
+
+    #[test]
+    fn test_body_parser_alt() {
+        let _ = body_parser_alt(RAW_RESPONSE_MOCK);
+
+        println!("{:#?}", body_parser_alt(RAW_RESPONSE_MOCK));
+
+        assert!(true);
+    }
+
     #[test]
     fn test_remove_non_alphanumeric() {
         assert_eq!(remove_non_alphanumeric("Hello World"), "HelloWorld");
@@ -51,17 +93,7 @@ mod tests {
 
     #[test]
     fn test_body_parser() {
-        let raw_response = r#"HTTP/1.1 200 OK
-Content-Type: application/json
-Content-Length: 12
-
-{   "hello": "world",
-    "name": "John Doe",
-    "age": "42",
-    "address": "123 Main St"
-}"#;
-
-        let body_map = body_parser(raw_response).unwrap();
+        let body_map = body_parser(RAW_RESPONSE_MOCK).unwrap();
         assert_eq!(body_map.get("name").unwrap(), "John Doe");
         assert_eq!(body_map.get("age").unwrap(), "42");
         assert_eq!(body_map.get("address").unwrap(), "123 Main St");
