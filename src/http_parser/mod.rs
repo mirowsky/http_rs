b636aca569451fda943c61721231929550bb8bdd
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-07-04 07:32:20 -0300

    feat(http_status_line_parser): implemented http_status_line_parser and included a unit test to verify it works

--- src/http_parser/mod.rs
+++ src/http_parser/mod.rs
@@ -3,4 +3,5 @@ pub mod body_parser_alt;
 pub mod header_parser;
 mod mocks;
 pub mod regex;
+pub mod status_line_parser;
 pub mod url_parser;
--- src/http_parser/status_line_parser.rs
+++ src/http_parser/status_line_parser.rs
@@ -1,3 +1,5 @@
+use color_eyre::{Report, Result};
+
 pub struct HttpStatusLine {
     // version of http protocol used.
     pub version: String,
@@ -7,6 +9,42 @@ pub struct HttpStatusLine {
     pub reason_phrase: String,
 }
 
-pub fn parse_status_line(status_line: &str) -> Result<HttpStatusLine> {
-    unimplemented!()
+pub fn parse_status_line(raw_response: &str) -> Result<HttpStatusLine> {
+    let mut status_line = raw_response
+        .lines()
+        .next()
+        .ok_or_else(|| Report::msg("No status line"))?
+        .split_whitespace();
+
+    Ok(HttpStatusLine {
+        version: status_line
+            .next()
+            .ok_or_else(|| Report::msg("No version"))?
+            .to_string(),
+        status_code: status_line
+            .next()
+            .ok_or_else(|| Report::msg("No status code"))?
+            .parse()
+            .map_err(|_| Report::msg("Could not parse status code"))?,
+        reason_phrase: status_line
+            .next()
+            .ok_or_else(|| Report::msg("No reason phrase"))?
+            .to_string(),
+    })
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::http_parser::mocks::response::RAW_RESPONSE_MOCK;
+
+    use super::*;
+    #[test]
+    fn test_parse_status_line() {
+        let status_line = parse_status_line(RAW_RESPONSE_MOCK).unwrap();
+
+        assert_eq!(status_line.version, "HTTP/1.1");
+        assert_eq!(status_line.status_code, 200);
+        assert_eq!(status_line.reason_phrase, "OK");
+    }
 }
