8abfb623e0690d213e56cab11f7643d50a7b8091
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-07-04 23:31:28 -0300

    feat(http_parser/regex): added a regex to extract ip addresses from requests

--- src/http_parser/regex.rs
+++ src/http_parser/regex.rs
@@ -4,4 +4,5 @@ use regex::Regex;
 lazy_static! {
     pub static ref BODY_REGEX: Regex = Regex::new(r"(?im)^\s*\{{1}[\s\S]*\}{1}$").unwrap();
     pub static ref HEADER_REGEX: Regex = Regex::new(r"(?im)^[^\s\W].*:{1}.*$").unwrap();
+    pub static ref IP_ADDRESS_REGEX: Regex = Regex::new(r"(?im)([0-9]{1,3}\.?){4}").unwrap();
 }
