63da49cfab45a0530ff66e8f2eccf9a1b0a7f1c2
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-07-06 23:35:54 -0300

    feat(http_version): impl with associated constants for each http version

--- src/http_version/mod.rs
+++ src/http_version/mod.rs
@@ -1,5 +1,7 @@
+#![allow(dead_code)]
+
 #[derive(Copy, Clone, Eq, Ord, Hash, PartialEq, PartialOrd)]
-pub enum HttpVersion {
+enum HttpVersion {
     Http09,
     Http10,
     Http11,
@@ -10,6 +12,23 @@ pub enum HttpVersion {
 
 impl Default for HttpVersion {
     fn default() -> Self {
-        HttpVersion::Http20
+        HttpVersion::HTTP_20
     }
+}
+
+impl HttpVersion {
+    // HTTP/0.9
+    pub const HTTP_09: HttpVersion = HttpVersion::Http09;
+
+    // HTTP/1.0
+    pub const HTTP_10: HttpVersion = HttpVersion::Http10;
+
+    // HTTP/1.1
+    pub const HTTP_11: HttpVersion = HttpVersion::Http11;
+
+    // HTTP/2.0
+    pub const HTTP_20: HttpVersion = HttpVersion::Http20;
+
+    // HTTP/3.0
+    pub const HTTP_30: HttpVersion = HttpVersion::Http30;
 }
