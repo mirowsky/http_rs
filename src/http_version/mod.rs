64fe94aece3d567b8e53437f24bfd389b32eccec
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-07-06 23:38:19 -0300

    feat(http_version): impl Debug trait for HttpVersion enum

--- src/http_version/mod.rs
+++ src/http_version/mod.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use std::fmt::Debug;
+
 #[derive(Copy, Clone, Eq, Ord, Hash, PartialEq, PartialOrd)]
 enum HttpVersion {
     Http09,
@@ -13,6 +15,21 @@ enum HttpVersion {
 impl Default for HttpVersion {
     fn default() -> Self {
         HttpVersion::HTTP_20
+    }
+}
+
+impl Debug for HttpVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Http09 => write!(f, "Http09"),
+            Self::Http10 => write!(f, "Http10"),
+            Self::Http11 => write!(f, "Http11"),
+            Self::Http20 => write!(f, "Http20"),
+            Self::Http30 => write!(f, "Http30"),
+            Self::NotHttp => {
+                panic!("HttpVersion::NotHttp is not a valid HttpVersion");
+            }
+        }
     }
 }
 
